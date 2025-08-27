b339d010c981b54f4623e1c6e63c18986714f90f
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-08-27 14:21:26 +0200

    Added coments and apikey validator

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
+
+# Error handeling if apikey is wrong or broken
+if not api_key:
+    print("Error: GEMINI_API_KEY not found in environment variables")
+    exit()
+
 client = genai.Client(api_key=api_key)
 
-
-
-
-
+# Generates content using Gemini api and display tokens used
 def main():
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -20,12 +23,6 @@ def main():
     response_tokens = usage_metadata.candidates_token_count
     print(response.text)
     print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
-
-
-
-
-
-
 
 if __name__ == "__main__":
     main()
