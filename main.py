936a9e153bc8caa9b5bc1012c0d7a900342dfc04
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-08-27 13:38:32 +0200

    API with tokens

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
+
+
+
 def main():
-    print("Hello from aibot!")
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    usage_metadata = response.usage_metadata
+    prompt_tokens = usage_metadata.prompt_token_count
+    response_tokens = usage_metadata.candidates_token_count
+    print(response.text)
+    print(f"Prompt tokens: {prompt_tokens}\nResponse tokens: {response_tokens}")
+
+
+
+
+
 
 
 if __name__ == "__main__":
