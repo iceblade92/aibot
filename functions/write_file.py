2dc16c77aca21b69c565bdb7a2ad52dc0dbc6cf6
iceblade92 <163899714+iceblade92@users.noreply.github.com>
2025-09-03 16:07:56 +0200

    added making file,directory and validation

---
+++ functions/write_file.py
@@ -0,0 +1,18 @@
+import os
+
+def write_file(working_directory, file_path, content):
+    try:
+        fullpath = os.path.join(working_directory, file_path)
+        abs_file = os.path.abspath(fullpath)
+        abs_workdir = os.path.abspath(working_directory)
+        directoryname = os.path.dirname(abs_file)
+        if not (abs_file == abs_workdir or abs_file.startswith(abs_workdir + os.sep)):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        else:
+            if directoryname and not os.path.exists(directoryname):
+                os.makedirs(directoryname, exist_ok=True)
+            with open(abs_file, "w") as f:
+                f.write(content)
+            return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except Exception as e:
+        return f"Error: {str(e)}"
--- tests.py
+++ tests.py
@@ -1,14 +1,11 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
-test1 = get_file_content("calculator", "main.py")
-test2 = get_file_content("calculator", "pkg/calculator.py")
-test3 = get_file_content("calculator", "/bin/cat")
-test4 = get_file_content("calculator", "pkg/does_not_exist.py")
+test1 = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+test2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+test3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 
 print(test1)
 
 print(test2)
 
 print(test3)
-
-print(test4)
